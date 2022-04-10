0c7be40407da0f604981329cc9237969d3397db8
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-04-10 10:34:33 -0500

    Project init

---
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/MinuteTrakApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group4.minuteTrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuteTrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuteTrakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/depaul/cdm/se452/group4/minuteTrak/MinuteTrakApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group4.minuteTrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuteTrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
