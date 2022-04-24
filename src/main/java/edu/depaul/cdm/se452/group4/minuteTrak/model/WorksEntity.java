571cd2cd38d31490e47177a943eea5fb9f9e627f
Thomas Le <le.thomas.09@gmail.com>
2022-04-24 14:18:48 -0700

    Update Table Relationship Fetch Type

--- src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/model/WorksEntity.java
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/model/WorksEntity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -55,7 +56,7 @@ public class WorksEntity {
   private LocalDateTime date;
 
   // /* Works(1) -> Employees(*) */
-  @OneToMany // (fetch = FetchType.EAGER)
+  @OneToMany (fetch = FetchType.EAGER)
   @JoinTable(name = "Works", joinColumns = { @joinColumn(name = "p_id", referencedColumnName = "e_id")})
   @ToString.Exclude
   private List<EmployeeEntity> employees;
