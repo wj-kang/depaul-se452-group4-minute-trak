98739d61e4e61a6b6075a1eb030b39212820c296
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-05-01 00:02:31 -0500

    Add mongodb Repositories

--- src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/mongo/MongoTestRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.depaul.cdm.se452.group4.minuteTrak.persistence.mongo;
-
-import edu.depaul.cdm.se452.group4.minuteTrak.model.mongo.MongoTest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MongoTestRepository extends MongoRepository<MongoTest, String> {
-
-}
