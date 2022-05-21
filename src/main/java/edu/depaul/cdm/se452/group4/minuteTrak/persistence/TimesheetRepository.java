1b8c4ef6b7c7525aab2588b981d3a1a78b80c213
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-05-20 22:47:32 -0500

    Update timesheet repository - add findAllByEmployee

--- src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimesheetRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimesheetRepository.java
@@ -1,10 +1,14 @@
 package edu.depaul.cdm.se452.group4.minuteTrak.persistence;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import edu.depaul.cdm.se452.group4.minuteTrak.model.EmployeeEntity;
 import edu.depaul.cdm.se452.group4.minuteTrak.model.TimesheetEntity;
 
 @Repository
 public interface TimesheetRepository extends JpaRepository<TimesheetEntity, Long> {
+
+  public List<TimesheetEntity> findAllByEmployee(EmployeeEntity employee);
 
 }
