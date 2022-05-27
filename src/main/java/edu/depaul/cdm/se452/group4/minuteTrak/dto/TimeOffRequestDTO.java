0cf4c1105f90b56f93db8cd785fbdbef2667737e
Thomas Le <le.thomas.09@gmail.com>
2022-05-26 21:43:17 -0700

    Add TimeOffRequestDTO

---
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/dto/TimeOffRequestDTO.java
@@ -0,0 +1,30 @@
+package edu.depaul.cdm.se452.group4.minuteTrak.dto;
+import java.time.LocalDate;
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TimeOffRequestDTO {
+
+    private long reqId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    private boolean isPaid;
+
+    private boolean isApproved;
+
+    private boolean isRejected;
+}
