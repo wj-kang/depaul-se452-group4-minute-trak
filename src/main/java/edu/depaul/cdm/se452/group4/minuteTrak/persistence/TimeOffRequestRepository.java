d3407b214ded2fdca376c2673876a734a627fa18
Thomas Le <le.thomas.09@gmail.com>
2022-05-28 06:53:38 -0700

    Update TimeOffRequestRepository

--- src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimeOffRequestRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimeOffRequestRepository.java
@@ -1,10 +1,15 @@
 package edu.depaul.cdm.se452.group4.minuteTrak.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import edu.depaul.cdm.se452.group4.minuteTrak.model.EmployeeEntity;
 import edu.depaul.cdm.se452.group4.minuteTrak.model.TimeOffRequestEntity;
 
 @Repository
 public interface TimeOffRequestRepository extends JpaRepository<TimeOffRequestEntity, Long> {
 
+    public List<TimeOffRequestEntity> findAllByEmployee(EmployeeEntity employee);
 }
