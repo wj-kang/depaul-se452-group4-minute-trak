36d148eacb0d870d622da83c37ae1002b83da3d2
Sardar Mustafa Khan <sardarm.khan942@gmail.com>
2022-06-01 22:49:21 -0500

    Merge branch 'dev' of https://github.com/wj-kang/depaul-se452-group4-minute-trak into dev

--- src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimeOffRequestRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group4/minuteTrak/persistence/TimeOffRequestRepository.java
@@ -1,8 +1,14 @@
 package edu.depaul.cdm.se452.group4.minuteTrak.persistence;
 
 import java.util.List;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 6735b08a92dbb32b9dc5c75588184d1b96e45ee6
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import edu.depaul.cdm.se452.group4.minuteTrak.model.EmployeeEntity;
 import edu.depaul.cdm.se452.group4.minuteTrak.model.TimeOffRequestEntity;
 
 @Repository
@@ -12,4 +18,5 @@ public interface TimeOffRequestRepository extends JpaRepository<TimeOffRequestEn
 
     public List<TimeOffRequestEntity> findAll();
 
+    public List<TimeOffRequestEntity> findAllByEmployee(EmployeeEntity employee);
 }
